45603f75b776fb4c471c3def8d9440860e3ff512
Daniel <danielromeroalaman@gmail.com>
2018-12-20 21:16:01 +0100

    more pages

--- e041-WebScraping/main/spiders/my_spider.py
+++ e041-WebScraping/main/spiders/my_spider.py
@@ -1,48 +1,33 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 from main.items import MainItem
 
 
 class AmazonSpider(scrapy.Spider):
+    link_count = 0
     name = 'amazon'
     allowed_domains = ['amazon.es']
-    search_for1, search_for2 = 'samsung+s8', 'samsung+s9'
+    search_for = 'samsung+s8'
     start_urls = [
-        'https://www.amazon.es/s/keywords='+search_for1,
-        'https://www.amazon.es/s/keywords='+search_for2
+        'https://www.amazon.es/s/keywords='+search_for
     ]
 
-
     def parse(self, response):
+        for link in response.xpath('//div[@class="a-row a-spacing-small"]/div/a/@href').extract():
+            yield scrapy.Request(url=link, callback=self.parse_items)
+
+    def parse_items(self, response):
         item = MainItem()
-        price_list, title_list, star_list, customer_reviews_list = [], [], [], []
 
-        for r in response.xpath('//span[@class="a-size-base a-color-price s-price a-text-bold"]/text()').extract():
-            price_list.append(r)
+        item['price'] = response.xpath('//span[@id="priceblock_ourprice"]/text()').extract()
+        item['title'] = response.xpath('//span[@id="productTitle"]/text()').extract()
+        item['stars'] = response.xpath('//a[@class="a-popover-trigger a-declarative"]/i/span[@class="a-icon-alt"]/text()').extract()
+        item['customer_reviews'] = response.xpath('//span[@id="acrCustomerReviewText"]/text()').extract()
 
-        for r in response.xpath(
-                '//h2[@class="a-size-medium s-inline  s-access-title  a-text-normal"]/text()').extract():
-            title_list.append(r)
+        yield {
+            'price': item['price'],
+            'title': item['title'],
+            'stars': item['stars'],
+            'customer_reviews': item['customer_reviews']
+        }
 
-        for r in response.xpath(
-                '//span/span/a/i/span[@class="a-icon-alt"]/text()').extract():
-            star_list.append(r)
-
-        for r in response.xpath(
-                '//div[@class="a-row a-spacing-mini"]/a[@class="a-size-small a-link-normal a-text-normal"]/text()').extract():
-            customer_reviews_list.append(r)
-
-        for i in range(len(price_list)):
-            item['prices'] = price_list[i]
-            item['titles'] = title_list[i]
-            item['stars'] = star_list[i]
-            item['customer_reviews'] = customer_reviews_list[i]
-
-            yield {
-                'prices': item['prices'],
-                'titles': item['titles'],
-                'stars': item['stars'],
-                'customer_reviews': item['customer_reviews']
-            }
 
